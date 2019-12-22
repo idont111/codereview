aecaffe852d4923ddc9793922768279316c0ddfa
idont111 <55505572+idont111@users.noreply.github.com>
2019-12-22 14:17:49 +0200

    Create task2.py

---
+++ chernyshaa/first/task2.py
@@ -0,0 +1,10 @@
+"""
+Задано список. Завдання: вивести кількість порожніх списків, які він містить.
+"""
+
+list=[programmer  other ]
+i=0
+for i in range(0, len(list)):
+    if list[i]==" ":
+        list[i]=list[]
+print(len.list[i])
